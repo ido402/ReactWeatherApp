ea01f295126c2137968d5949712876bd453fc8f5
Ido Doron <ido@modifly.com>
2017-07-17 13:43:56 -0500

    first weather app check in

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+
+import axios from 'axios';
+
+const API_KEY = '5e2196505aeed7d24a1873ca24469fbe';
+const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; //using template string ES6
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(cityName){
+
+    const url = `${ROOT_URL}&q=${cityName},us`;
+    const request = axios.get(url);
+
+    console.log('Request is: ',request);
+
+    return{
+        type: FETCH_WEATHER,
+        payload: request.data
+    };
+}
---
+++ src/containers/search_bar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index.js';
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+        //binding the input change action to the searchbar and replace on input change.
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event){
+        //console.log(event.target.value);
+        this.setState({ term:event.target.value });
+    }
+
+    onFormSubmit(event){
+        //blocks the user to type once clicked "enter" while on the input bar
+        event.preventDefault();
+        //will call an action creator 
+        this.props.fetchWeather(this.state.term);
+        this.setState({term:''});
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input
+                    placeholder="get a five-day forecase in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchWeather},dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
